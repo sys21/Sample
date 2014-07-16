640e964df2308d58c2c6ea9ecdf7949738c476cc
s_tateno <syu39310@gmail.com>
2014-07-17 01:06:07 +0900

    EditText追加

--- src/com/sample/MainFragment.java
+++ src/com/sample/MainFragment.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * @author 修平
@@ -18,9 +21,9 @@ import android.widget.Button;
 public class MainFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		final View v = inflater.inflate(R.layout.fragment_main, container);
+		final View view = inflater.inflate(R.layout.fragment_main, container);
 		// SubActivityへの遷移ボタン
-		final Button btn = (Button) v.findViewById(R.id.btn_move_sub_activity);
+		final Button btn = (Button) view.findViewById(R.id.btn_move_sub_activity);
 		btn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -28,6 +31,20 @@ public class MainFragment extends Fragment {
 				final Intent intent = new Intent(getActivity(), SubActivity.class);
 				// 次画面のアクティビティ起動
 				startActivity(intent);
+			}
+		});
+		// EditText
+		final EditText editText = (EditText) view.findViewById(R.id.edit_text);
+		editText.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(final View v, final boolean hasFocus) {
+				if (hasFocus) {
+					// フォーカス時
+				} else {
+					// フォーカスアウト時
+					final TextView textView = (TextView) view.findViewById(R.id.edit_text_label);
+					textView.setText(((EditText) v).getText().toString());
+				}
 			}
 		});
 		return super.onCreateView(inflater, container, savedInstanceState);
